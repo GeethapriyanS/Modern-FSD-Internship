7f96e55ebae6b5fed285219ce5a7563e22caa24b
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2025-07-04 10:38:55 +0530

    Changes added

--- Day2/GuessNumber/index.js
+++ Day2/GuessNumber/index.js
@@ -1,55 +1,59 @@
-const againButton = document.querySelector('.again');
-const num=document.querySelector('.number');
-const guess=document.querySelector('.guess');
-const checkBtn=document.querySelector('.check');
-const message=document.querySelector('.message');
-const score=document.querySelector('.score');
-const highscore=document.querySelector('.highscore');
+const againButton = document.querySelector('.btn-reset');
+const num = document.querySelector('.secret-number');
+const guess = document.querySelector('.input-guess');
+const checkBtn = document.querySelector('.btn-submit');
+const message = document.querySelector('.status-message');
+const score = document.querySelector('.score-value');
+const highscore = document.querySelector('.highscore-value');
 
-let random=Math.trunc(Math.random()*20)+1;
-let scores=20;
+let random = Math.trunc(Math.random() * 20) + 1;
+let scores = 20;
 console.log(random);
 
-const checkfunc=()=>{
-    const guessValue=Number(guess.value);
-    if(!guessValue){
-        message.textContent='No Number!';
-    }else if(guessValue===random){
-        message.textContent=' Correct Number!';
-        num.textContent=random;
-        document.body.style.backgroundColor='green';
-        num.style.width='30rem';
-        if(scores>Number(highscore.textContent)){
-            highscore.textContent=scores;
+const checkfunc = () => {
+    const guessValue = Number(guess.value);
+    if (!guessValue) {
+        message.textContent = 'No Number!';
+    } else if (guessValue === random) {
+        message.textContent = ' Correct Number!';
+        num.textContent = random;
+        document.body.style.backgroundColor = 'green';
+        num.style.width = '30rem';
+        if (scores > Number(highscore.textContent)) {
+            highscore.textContent = scores;
         }
-    }else if(guessValue>random){
-        if(scores>1){
-            message.textContent=' Too High!';
+    } else if (guessValue > random) {
+        if (scores > 1) {
+            message.textContent = ' Too High!';
             scores--;
-            score.textContent=scores;
-        }else{
-            message.textContent=' You lost the game!';
-            score.textContent=0;
+            score.textContent = scores;
+        } else {
+            message.textContent = ' You lost the game!';
+            score.textContent = 0;
         }
-    }else if(guessValue<random){
-        if(scores>1){
-            message.textContent=' Too Low!';
+    } else if (guessValue < random) {
+        if (scores > 1) {
+            message.textContent = ' Too Low!';
             scores--;
-            score.textContent=scores;
-        }else{
-            message.textContent=' You lost the game!';
-            score.textContent=0;
+            score.textContent = scores;
+        } else {
+            message.textContent = ' You lost the game!';
+            score.textContent = 0;
         }
     }
-}
-const againfunc=()=>{
-    scores=20;
-    random=Math.trunc(Math.random()*20)+1;
+};
+
+const againfunc = () => {
+    scores = 20;
+    random = Math.trunc(Math.random() * 20) + 1;
     console.log(random);
-    score.textContent=scores;
-    message.textContent='Start guessing...';
-    num.textContent='?';
-    guess.value='';
-    document.body.style.backgroundColor='black';
-    num.style.width='15rem';
-}
+    score.textContent = scores;
+    message.textContent = 'Start guessing...';
+    num.textContent = '?';
+    guess.value = '';
+    document.body.style.backgroundColor = 'black';
+    num.style.width = '15rem';
+};
+
+checkBtn.addEventListener('click', checkfunc);
+againButton.addEventListener('click', againfunc);
